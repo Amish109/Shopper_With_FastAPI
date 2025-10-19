acc70989baacfa9de383afa4a8ac8d8b4a5aac45
Amish109 <tiwariamish786@gmail.com>
2025-10-19 18:10:28 +0530

    base_proj

--- fast-api/app/routes/user_router.py
+++ fast-api/app/routes/user_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from app.controllers.user_controller import sign_up, sign_in, sign_out, check_cookie
 from app.middleware.auth_jwt import verify_jwt
 
-router = APIRouter(prefix="/user", tags=["User"])
+router = APIRouter(prefix="/api/user", tags=["User"])
 
 router.post("/sign-up")(sign_up)
 router.post("/sign-in")(sign_in)
